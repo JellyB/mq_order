3a9560e325e19c80da877db97cca6c39280f343f
jellyb <jelly_b@126.com>
2019-10-31 16:17:47 +0800

    mq 实现分布式事务

---
+++ src/main/java/com/kljs/rabbitmq/order/util/deserializer/IntegerDeserializer.java
@@ -0,0 +1,23 @@
+package com.kljs.rabbitmq.order.util.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mysql.jdbc.StringUtils;
+
+import java.io.IOException;
+
+public class IntegerDeserializer extends JsonDeserializer<Integer> {
+    public static final String CLOSED_WARNING = "close_exceed_max_int_warning";
+
+    @Override
+    public Integer deserialize(JsonParser parser, DeserializationContext ctxt)
+            throws IOException {
+        String data = ((JsonNode) parser.getCodec().readTree(parser)).asText();
+        if (!StringUtils.isNullOrEmpty(data) && data.length() == 10) {
+        }
+
+        return StringUtils.isNullOrEmpty(data) ? null : Integer.parseInt(data) ;
+    }
+}
---
+++ src/main/java/com/kljs/rabbitmq/order/util/deserializer/LocalDateTimeDeserializer.java
@@ -0,0 +1,23 @@
+package com.kljs.rabbitmq.order.util.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.kljs.rabbitmq.order.util.VTime;
+import com.mysql.jdbc.StringUtils;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public final class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+    @Override
+    public LocalDateTime deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        String dateTimeStr = ((JsonNode) parser.getCodec().readTree(parser)).asText();
+        if (!StringUtils.isNullOrEmpty(dateTimeStr)) {
+            return VTime.toLocalDateTime(new Long(dateTimeStr));
+        }
+        return LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+}
